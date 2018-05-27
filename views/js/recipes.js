59e70d83461cd94aca16ff20e3c835952be247a1
Tehmo3 <jasper.b.miles@gmail.com>
2018-05-27 21:41:11 +1000

    Document.ready()

--- views/js/recipes.js
+++ views/js/recipes.js
@@ -2,9 +2,11 @@ items = ["hot", "cold", "sweet", "savoury", "salty", "chocolate", "bitter",
 "spicy", "crunchy", "greasy", "creamy", "chewy", "vegetables", "meat",
 "comfort", "snack", "meal", "breakfast", "lunch", "dinner", "cake", "biscuit",
 "baked", "crispy", "bread", "dessert"],
-$('recipeSearch').selectize({
-  delimiter: ',',
-  create: true,
-  items: items,
 
-});
+$(document).ready(() => {
+  $('recipeSearch').selectize({
+    delimiter: ',',
+    create: true,
+    items: items,
+  });
+})
