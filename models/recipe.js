f0adc24e043bd646f915980a073e844f279f2ca2
Tehmo3 <jasper.b.miles@gmail.com>
2018-05-27 21:21:21 +1000

    initial recipe tag list

--- models/recipe.js
+++ models/recipe.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const
+
 const recipeSchema = mongoose.Schema({
   id: Number,
   name: String,
@@ -12,7 +14,15 @@ const recipeSchema = mongoose.Schema({
     quantity: Number,
     unit: String
   }],
-  method: [String]
+  method: [String],
+  tags: [{
+    type: String,
+    enum: ["hot", "cold", "sweet", "savoury", "salty", "chocolate", "bitter",
+    "spicy", "crunchy", "greasy", "creamy", "chewy", "vegetables", "meat",
+    "comfort", "snack", "meal", "breakfast", "lunch", "dinner", "cake", "biscuit",
+    "baked", "crispy", "bread", "dessert", ""],
+    required: "Please specify at least one tag"
+  }]
 })
 
 module.exports = mongoose.model('Recipe', recipeSchema);
